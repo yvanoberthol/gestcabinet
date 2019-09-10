d5dd1b953994d1b45bdfe58a039c3f8dabda9ddc
yvano berthol <yvanoberthol@gmail.com>
2019-09-10 16:02:06 +0200

    utilitaire de gestion des dates et de la securite

--- src/main/java/com/yvanscoop/gestcabinet/utility/DateUtils.java
+++ src/main/java/com/yvanscoop/gestcabinet/utility/DateUtils.java
@@ -1,4 +1,17 @@
 package com.yvanscoop.gestcabinet.utility;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class DateUtils {
+
+    public final static Date gererDate(Date jour){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdf.setLenient(false);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(jour);
+        cal.add(Calendar.DATE,1);
+        return cal.getTime();
+    }
 }
--- src/main/java/com/yvanscoop/gestcabinet/utility/SecurityUtility.java
+++ src/main/java/com/yvanscoop/gestcabinet/utility/SecurityUtility.java
@@ -16,13 +16,13 @@ public class SecurityUtility {
         return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
     }
 
-    @Bean
-    public static String randomPassword() {
+
+    public static String randomPassword(int poid) {
         String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
 
-        while (salt.length() < 18) {
+        while (salt.length() < poid) {
             int index = (int) (rnd.nextFloat() * SALTCHARS.length());
             salt.append(SALTCHARS.charAt(index));
         }
